959c815eb8fd4ec94a23338b6e749831e1c6f6c0
Graeme McCutcheon <graememcc_firefox@graeme-online.co.uk>
2014-04-02 18:39:12 +0100

    Add 'curryWithArity' function

--- base.js
+++ base.js
@@ -5,9 +5,10 @@
   var makeModule = function(require, exports) {
 
     /*
-     * curry: take a function f, and curry up to the function's arity, as defined by the
-     *        function's length property. Functions of arity 0 and 1 are returned unharmed.
-     *        Arguments are curried left to right.
+     * curry: take a function fn, and curry up to the function's arity, as defined by the
+     *        function's length property. The curried function returned will have length 1,
+     *        unless the original function had length 0, in which case the returned function
+     *        will have length 0 too. Arguments are curried left to right.
      *
      * curry will ultimately call the underlying function with a null execution context. Use
      * Function.prototype.bind beforehand if you need to curry with a specific context.
@@ -20,17 +21,51 @@
      *   f(1, 2); // calls the original function with a=1, b=2
      *
      * If the curried function is called with superfluous arguments, they will be discarded.
-     * This avoids unexpected behaviours triggered by supplying optional arguments.
+     * This avoids unexpected behaviour triggered by supplying optional arguments. Functions accepting
+     * optional arguments effectively represent a family of functions with different type-signatures,
+     * so each variant should be treated seperately.
+     *
+     * If you need a function to accept a specific number of arguments, where that number is different
+     * from the function's length property, use curryWithArity instead.
      */
 
-    var curry = function(f) {
-      // Define curry with length 1, so don't leak our optional arg to function length
-      var length = (arguments.length === 1) ? f.length : arguments[1];
+    var curry = function(fn) {
+      return curryWithArity(fn.length, fn);
+    };
 
-      if (length < 2)
-        return f;
 
-      var first = function(a) {
+    /*
+     * curryWithArity: take an arity and a function fn, and curry up to the specified arity, regardless of the
+     *                 function's length. Note in particular, a function can be curried to a length greater
+     *                 than its arity. Arguments are curried left to right. The returned function will have
+     *                 length 1, unless the arity requested was 0, in which case the length will be zero.
+     *
+     * curryWithArity will ultimately call the underlying function with a null execution context. Use
+     * Function.prototype.bind beforehand if you need to curry with a specific context.
+     *
+     * Each curried function will have an arity of 1, but will accept > 1 arguments. If the number
+     * of arguments supplied is equal to the number of outstanding arguments, the
+     * underlying function will be called. For example,
+     *   var f = curryWithArity(2, function(a, b) {...});
+     *   f(1); // returns a function that awaits a value for the b parameter
+     *   f(1, 2); // calls the original function with a=1, b=2
+     *
+     * If the curried function is called with superfluous arguments, they will be discarded.
+     * This avoids unexpected behaviour triggered by supplying optional arguments. Functions accepting
+     * optional arguments effectively represent a family of functions with different type-signatures,
+     * so each variant should be treated seperately.
+     */
+
+    var curryWithArity = function(length, fn) {
+      // Handle the special case of length 0
+      if (length === 0) {
+        return function() {
+          // Don't simply return fn: need to discard any arguments
+          return fn();
+        };
+      }
+
+      var curried = function(a) {
         var args = [].slice.call(arguments);
         var argsNeeded = length - args.length;
 
@@ -41,15 +76,15 @@
 
         // If we have enough arguments, call the underlying function
         if (args.length === length)
-          return f.apply(null, args);
+          return fn.apply(null, args);
 
         // We don't have enough arguments. Bind those that we already have
-        var newFn = f.bind.apply(f, [null].concat(args));
+        var newFn = fn.bind.apply(fn, [null].concat(args));
         var argsNeeded = length - args.length;
 
         // Continue currying if we can't yet return a function of length 1
         if (argsNeeded > 1)
-          return curry(newFn, argsNeeded);
+          return curryWithArity(argsNeeded, newFn);
 
         // The trivial case
         return function(b) {
@@ -57,13 +92,22 @@
         };
       };
 
-      return first;
+      return curried;
     };
 
 
     var exported = {
-      curry: curry
+      curry: curry,
+      curryWithArity: curryWithArity
     };
+
+
+    for (var k in exported) {
+      if (!exported.hasOwnProperty(k))
+        continue;
+
+      exported[k] = curry(exported[k]);
+    }
 
 
     module.exports = exported;
--- test/testBase.js
+++ test/testBase.js
@@ -16,7 +16,7 @@
 
 
     describe('Base exports', function() {
-      var expectedFunctions = ['curry'];
+      var expectedFunctions = ['curry', 'curryWithArity'];
 
       // Automatically generate existence tests for each expected function
       expectedFunctions.forEach(function(f) {
@@ -28,20 +28,6 @@
 
     describe('curry', function() {
       var curry = base.curry;
-
-      it('curry returns a function of length 0 unharmed', function() {
-        var f = function() {};
-        var curried = curry(f);
-        expect(curried).to.equal(f);
-      });
-
-
-      it('curry returns a function of length 1 unharmed', function() {
-        var f = function(x) {return x;};
-        var curried = curry(f);
-        expect(curried).to.equal(f);
-      });
-
 
       // We shall test curry with binary, ternary, and quarternary functions
       var testFuncs = [
@@ -60,13 +46,100 @@
       });
 
 
-      it('Curried function called with anticipated arguments when called with more than required', function() {
+      it('Underlying function called with anticipated arguments when curried function called with superfluous arguments', function() {
         var f = function(a, b) {f.args = [].slice.call(arguments);};
         f.args = null;
         var curried = curry(f);
         var args = [1, 2, 4, 'a'];
         curried(args[0], args[1], args[2], args[3]);
         expect(f.args).to.deep.equal(args.slice(0, f.length));
+      });
+    });
+
+
+    describe('curryWithArity', function() {
+      var curryWithArity = base.curryWithArity;
+      var fromCharCodes = String.fromCharCode;
+
+      it('curryWithArity returns a function of length 1 when a function of length 0 is curried to a length > 0', function() {
+        var f = function() {};
+        var curried = curryWithArity(1, f);
+        expect(curried.length).to.equal(1);
+      });
+
+
+      it('curryWithArity returns a function of length 0 when a function of length > 0 is curried to length 0', function() {
+        var f = function(a, b, c) {};
+        var curried = curryWithArity(0, f);
+        expect(curried.length).to.equal(0);
+      });
+
+
+      // One of the motivating ideas of curryWithArity relates to methods of standard objects
+      // that have a given length, but accept fewer or more arguments.
+      // For example, Array.reduce has a standard mandated length of 1, but is normally
+      // called with 2 parameters.
+      //
+      // We use String.fromCharCode in our tests. String.fromCharCode accepts any number of parameters
+      // and shouldn't depend on this === String
+      it('Sanity check: String.fromCharCode has length 1', function() {
+        expect(String.fromCharCode.length).to.equal(1);
+      });
+
+
+      it('Sanity check: String.fromCharCode can be called without an execution context', function() {
+        var code = 65;
+        var fn = function() {
+          return fromCharCodes(code);
+        };
+        expect(fn).to.not.throw(Error);
+        expect(fn()).to.equal(String.fromCharCode(code));
+      });
+
+
+      // We shall test curryWithArity with binary, and ternary versions
+      var testFuncs = [
+        {f: fromCharCodes, args: [65, 66], message: 'Arbitrary length function curried as a binary function'},
+        {f: fromCharCodes, args: [65, 66, 67], message: 'Arbitrary length function curried as a ternary function'}
+      ];
+
+      testFuncs.forEach(function(testData) {
+        var fn = testData.f;
+        var args = testData.args;
+        var message = testData.message;
+
+        var curried = curryWithArity(args.length, fn);
+        testCurriedFunction(curried, args, fn, args, message);
+      });
+
+
+      // curryWithArity should itself be curried
+      // We use an IIFE here to avoid an 'it' wrapped in an 'it'
+      (function() {
+        var fn = function(a, b) {return a + b;};
+        var args = {firstArgs: [fn.length, fn], thenArgs: [41, 1]};
+        var message = 'curryWithArity';
+        testCurriedFunction(curryWithArity, [fn.length, fn], curryWithArity, args, message);
+      }());
+
+
+      it('Underlying function called with anticipated arguments when curried function called with superfluous arguments', function() {
+        var f = function(a, b) {f.args = [].slice.call(arguments);};
+        f.args = null;
+        var curried = curryWithArity(2, f);
+        var args = [1, 2, 4, 'a'];
+        curried(args[0], args[1], args[2], args[3]);
+        expect(f.args).to.deep.equal(args.slice(0, f.length));
+      });
+
+
+      it('Underlying function can be curried with more arguments than underlying function length', function() {
+        var f = function(a, b) {f.args = [].slice.call(arguments);};
+        f.args = null;
+        var curried = curryWithArity(5, f);
+        var args = [1, 2, 4, 'a', null];
+        curried(args[0])(args[1])(args[2])(args[3])(args[4]);
+        expect(f.args).to.deep.equal(args);
       });
     });
   };
