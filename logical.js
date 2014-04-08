fa499e9535f7f5264974197c94db7feb84f08a83
Graeme McCutcheon <graememcc_firefox@graeme-online.co.uk>
2014-04-08 16:45:25 +0100

    Add 'andPred' function

--- logical.js
+++ logical.js
@@ -68,8 +68,28 @@
     };
 
 
+    /*
+     * andPred: takes two unary predicate functions, and returns a new unary function that returns true
+     *          only if both the supplied predicates return true for the given argument. It will short-circuit:
+     *          when the first predicate function returns false, the second will not be called.
+     *
+     */
+
+    var andPred = curry(function(pred1, pred2) {
+      var predLen1 = getRealArity(pred1);
+      var predLen2 = getRealArity(pred2);
+      if (predLen1 !== 1 || predLen2 !== 1)
+         throw new TypeError('andPred called with function of arity ' + (predLen1 !== 1 ? predLen1 : predLen2));
+
+      return curry(function(x) {
+        return pred1(x) && pred2(x);
+      });
+    });
+
+
     var exported = {
       and: and,
+      andPred: andPred,
       not: not,
       notPred: notPred,
       or: or,
--- test/testLogical.js
+++ test/testLogical.js
@@ -18,7 +18,7 @@
 
     describe('Logical exports', function() {
       var expectedFunctions = ['not', 'and', 'or', 'xor',
-                               'notPred'];
+                               'notPred', 'andPred'];
 
       // Automatically generate existence tests for each expected function
       expectedFunctions.forEach(function(f) {
@@ -117,34 +117,28 @@
       });
 
 
-      it('notPred throws when called with a function of arity 0', function() {
-        var predicate = function() {};
-        var fn = function() {
-          notPred(predicate);
+      // Utility function for test generation
+      var makePredicateArityTest = function(funcUnderTest, badArity) {
+        return function() {
+          var fn = function() {
+            funcUnderTest(badArity);
+          };
+
+          expect(fn).to.throw(TypeError);
         };
-
-        expect(fn).to.throw(TypeError);
-      });
+      };
 
 
-      it('notPred throws when called with a function of arity > 1', function() {
-        var predicate = function(x, y) {};
-        var fn = function() {
-          notPred(predicate);
-        };
-
-        expect(fn).to.throw(TypeError);
-      });
+      it('notPred throws when called with a function of arity 0',
+          makePredicateArityTest(notPred, function() {}));
 
 
-      it('notPred throws when called with a curried function of arity > 1', function() {
-        var predicate = curry(function(x, y) {});
-        var fn = function() {
-          notPred(predicate);
-        };
+      it('notPred throws when called with a function of arity > 1',
+          makePredicateArityTest(notPred, function(x, y) {}));
 
-        expect(fn).to.throw(TypeError);
-      });
+
+      it('notPred throws when called with a curried function of arity > 1',
+          makePredicateArityTest(notPred, curry(function(x, y) {})));
 
 
       it('notPred works as expected (1)', function() {
@@ -163,6 +157,102 @@
         expect(negated.length).to.equal(1);
         expect(negated('a')).to.equal(true);
       });
+    });
+
+
+    // Utility functions for test generation
+    var makeBinaryPredicateArityTest = function(funcUnderTest, pred1, pred2) {
+      return function() {
+        var fn = function() {
+          funcUnderTest(pred1, pred2);
+        };
+
+        expect(fn).to.throw(TypeError);
+      };
+    };
+
+
+    var makeBinaryPredicateTest = function(funcUnderTest, pred1, pred2, expected) {
+      return function() {
+        expect(funcUnderTest(pred1, pred2)('a')).to.equal(expected);
+      };
+    };
+
+
+    var makePredicateShortCircuitTest = function(funcUnderTest, val1, val2) {
+      var pred1 = function(x) {
+        return val1;
+      };
+
+      var pred2 = function(x) {
+        pred2.called = true;
+        return val2;
+      };
+
+      return function() {
+        pred2.called = false;
+        funcUnderTest(pred1, pred2)('a');
+        expect(pred2.called).to.equal(false);
+      };
+    };
+
+
+    // All the boolean operator tests have the same template
+    var makeBinaryPredicateTestFixture = function(prop, truthTable) {
+      var functionUnderTest = logical[prop];
+      var f0 = function() {};
+      var f2 = function(x, y) {};
+
+      it(prop + ' throws when called with a function of arity 0 (1)',
+          makeBinaryPredicateArityTest(functionUnderTest, f0, constantTrue));
+
+
+      it(prop + ' throws when called with a function of arity 0 (2)',
+          makeBinaryPredicateArityTest(functionUnderTest, constantTrue, f0));
+
+
+      it(prop + ' throws when called with a function of arity > 1 (1)',
+          makeBinaryPredicateArityTest(functionUnderTest, f2, constantTrue));
+
+
+      it(prop + ' throws when called with a function of arity > 1 (2)',
+          makeBinaryPredicateArityTest(functionUnderTest, constantTrue, f2));
+
+
+      it(prop + ' throws when called with a curried function of arity > 1 (1)',
+          makeBinaryPredicateArityTest(functionUnderTest, curry(f2), constantTrue));
+
+
+      it(prop + ' throws when called with a curried function of arity > 1 (2)',
+          makeBinaryPredicateArityTest(functionUnderTest, constantTrue, curry(f2)));
+
+
+      truthTable.forEach(function(test, i) {
+        var indexString = ' (' + (i + 1) + ')';
+        it(prop + ' works as expected' + indexString,
+            makeBinaryPredicateTest(functionUnderTest, test.pred1, test.pred2, test.expected));
+
+        if (test.shortCircuits) {
+          // Note, we deliberately call the predicate functions now—we need the values they return
+          it(prop + ' correctly short-circuits' + indexString,
+              makePredicateShortCircuitTest(functionUnderTest, test.pred1(), test.pred2()));
+        }
+
+        var curryArgs = {firstArgs: [test.pred1, test.pred2], thenArgs: ['a']};
+        testCurriedFunction(prop + ' is curried' + indexString, functionUnderTest, curryArgs);
+      });
+    };
+
+
+    describe('andPred', function() {
+      var truthTable = [
+        {pred1: constantFalse, pred2: constantFalse, expected: false, shortCircuits: true},
+        {pred1: constantFalse, pred2: constantTrue, expected: false, shortCircuits: true},
+        {pred1: constantTrue, pred2: constantFalse, expected: false, shortCircuits: false},
+        {pred1: constantTrue, pred2: constantTrue, expected: true, shortCircuits: false}
+      ];
+
+      makeBinaryPredicateTestFixture('andPred', truthTable);
     });
   };
 
