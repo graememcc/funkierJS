a3b805974337e95377a6769bf70044ec7b79381a
Graeme McCutcheon <graememcc_firefox@graeme-online.co.uk>
2014-07-11 12:55:39 +0100

    Add help for logical.js

--- logical.js
+++ logical.js
@@ -10,112 +10,163 @@
     var funcUtils = require('./funcUtils');
     var checkFunction = funcUtils.checkFunction;
 
+    var utils = require('./utils');
+    var defineValue = utils.defineValue;
 
-    /*
-     * not: returns the boolean negation of the given argument
-     *
-     */
 
-    var not = curry(function(x) {
+    var not = defineValue(
+      'name: not',
+      'signature: x: boolean',
+      'classification: logical',
+      '',
+      'A wrapper around the logical not (!) operator. Returns the logical negation of the given argument.',
+      '--',
+      'not(true); // false',
+      curry(function(x) {
       return !x;
-    });
+      })
+    );
 
 
-    /*
-     * and: returns and (&&) applied to the given arguments.
-     *      Note: due to eager evaluation, this will not short-circuit.
-     *
-     */
-
-    var and = curry(function(x, y) {
-      return x && y;
-    });
-
-
-    /*
-     * or: returns or (||) applied to the given arguments.
-     *     Note: due to eager evaluation, this will not short-circuit.
-     *
-     */
-
-    var or = curry(function(x, y) {
-      return x || y;
-    });
+    var and = defineValue(
+      'name: and',
+      'signature: x: boolean, y: boolean',
+      'classification: logical',
+      '',
+      'A wrapper around the logical and (&&) operator. Returns the logical and of the given arguments.',
+      '--',
+      'and(false, true); // false',
+      curry(function(x, y) {
+        return x && y;
+      })
+    );
 
 
-    /*
-     * xor: returns logical xor of the given arguments.
-     *
-     */
-
-    var xor = curry(function(x, y) {
-      return x == y ? false : true;
-    });
-
-
-    /*
-     * notPred: takes an unary predicate function, and returns a new unary function that calls
-     *          the original function with the given argument, and returns the negation of the result
-     *
-     */
-
-    var notPred = function(pred) {
-      pred = checkFunction(pred, {arity: 1, message: 'Predicate must be a function of arity 1'});
-
-      return curry(function(x) {
-        return !pred(x);
-      });
-    };
+    var or = defineValue(
+      'name: or',
+      'signature: x: boolean, y: boolean',
+      'classification: logical',
+      '',
+      'A wrapper around the logical o& (||) operator. Returns the logical or of the given arguments.',
+      '--',
+      'or(false, true); // true',
+      curry(function(x, y) {
+        return x || y;
+      })
+    );
 
 
-    /*
-     * andPred: takes two unary predicate functions, and returns a new unary function that returns true
-     *          only if both the supplied predicates return true for the given argument. It will short-circuit:
-     *          when the first predicate function returns false, the second will not be called.
-     *
-     */
-
-    var andPred = curry(function(pred1, pred2) {
-      pred1 = checkFunction(pred1, {arity: 1, message: 'First predicate must be a function of arity 1'});
-      pred2 = checkFunction(pred2, {arity: 1, message: 'Second predicate must be a function of arity 1'});
-
-      return curry(function(x) {
-        return pred1(x) && pred2(x);
-      });
-    });
+    var xor = defineValue(
+      'name: xor',
+      'signature: x: boolean, y: boolean',
+      'classification: logical',
+      '',
+      'Returns the logical xor of the given arguments.',
+      '--',
+      'xor(false, true); // true',
+      'xor(true, true); // false',
+      curry(function(x, y) {
+        return x == y ? false : true;
+      })
+    );
 
 
-    /*
-     * orPred: takes two unary predicate functions, and returns a new unary function that returns true
-     *         if one or both the supplied predicates return true for the given argument. It will short-circuit:
-     *         when the first predicate function returns true, the second will not be called.
-     *
-     */
+    var notPred = defineValue(
+      'name: notPred',
+      'signature: f: function',
+      'classification: logical',
+      '',
+      'Takes a unary predicate function, and returns a new unary function that, when called, will call',
+      'the original function with the given argument, and return the negated result.',
+      '',
+      'Throws a TypeError if f is not a function, or if it has an arity other than 1.',
+      '--',
+      'var c = constant(true);',
+      'var f = notPred(c);',
+      'f("foo"); // false',
+      curry(function(pred) {
+        pred = checkFunction(pred, {arity: 1, message: 'Predicate must be a function of arity 1'});
 
-    var orPred = curry(function(pred1, pred2) {
-      pred1 = checkFunction(pred1, {arity: 1, message: 'First predicate must be a function of arity 1'});
-      pred2 = checkFunction(pred2, {arity: 1, message: 'Second predicate must be a function of arity 1'});
-
-      return curry(function(x) {
-        return pred1(x) || pred2(x);
-      });
-    });
+        return curry(function(x) {
+          return !pred(x);
+        });
+      })
+    );
 
 
-    /*
-     * xorPred: takes two unary predicate functions, and returns a new unary function that returns true
-     *          if exactly one of the supplied predicates return true for the given argument.
-     *
-     */
+    var andPred = defineValue(
+      'name: andPred',
+      'signature: f: function, g: function',
+      'classification: logical',
+      '',
+      'Takes two unary predicate functions, and returns a new unary function that, when called, will call',
+      'the original functions with the given argument, and and their results, returning this value.',
+      '',
+      'Throws a TypeError if either argument is not a function of arity 1.',
+      '--',
+      'var c = constant(true);',
+      'var d = constant(false);',
+      'var f = andPred(c, d);',
+      'f("foo"); // false',
+      curry(function(pred1, pred2) {
+        pred1 = checkFunction(pred1, {arity: 1, message: 'First predicate must be a function of arity 1'});
+        pred2 = checkFunction(pred2, {arity: 1, message: 'Second predicate must be a function of arity 1'});
 
-    var xorPred = curry(function(pred1, pred2) {
-      pred1 = checkFunction(pred1, {arity: 1, message: 'First predicate must be a function of arity 1'});
-      pred2 = checkFunction(pred2, {arity: 1, message: 'Second predicate must be a function of arity 1'});
+        return curry(function(x) {
+          return pred1(x) && pred2(x);
+        });
+      })
+    );
 
-      return curry(function(x) {
-        return xor(pred1(x), pred2(x));
-      });
-    });
+
+    var orPred = defineValue(
+      'name: orPred',
+      'signature: f: function, g: function',
+      'classification: logical',
+      '',
+      'Takes two unary predicate functions, and returns a new unary function that, when called, will call',
+      'the original functions with the given argument, and or their results, returning this value.',
+      '',
+      'Throws a TypeError if either argument is not a function of arity 1.',
+      '--',
+      'var c = constant(true);',
+      'var d = constant(false);',
+      'var f = orPred(c, d);',
+      'f("foo"); // true',
+      curry(function(pred1, pred2) {
+        pred1 = checkFunction(pred1, {arity: 1, message: 'First predicate must be a function of arity 1'});
+        pred2 = checkFunction(pred2, {arity: 1, message: 'Second predicate must be a function of arity 1'});
+
+        return curry(function(x) {
+          return pred1(x) || pred2(x);
+        });
+      })
+    );
+
+
+    var xorPred = defineValue(
+      'name: xorPred',
+      'signature: f: function, g: function',
+      'classification: logical',
+      '',
+      'Takes two unary predicate functions, and returns a new unary function that, when called, will call',
+      'the xoriginal functions with the given argument, and xor their results, returning this value.',
+      '',
+      'Throws a TypeErrxor if either argument is not a function of arity 1.',
+      '--',
+      'var c = constant(true);',
+      'var d = constant(true);',
+      'var f = xorPred(c, d);',
+      'f("foo"); // false',
+      curry(function(pred1, pred2) {
+        pred1 = checkFunction(pred1, {arity: 1, message: 'First predicate must be a function of arity 1'});
+        pred2 = checkFunction(pred2, {arity: 1, message: 'Second predicate must be a function of arity 1'});
+
+        return curry(function(x) {
+          return xor(pred1(x), pred2(x));
+        });
+      })
+    );
 
 
     var exported = {
