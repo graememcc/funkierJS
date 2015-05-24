a5e6fdd972477cd7ab482f1f5461909818e33ecb
Graeme McCutcheon <graememcc_firefox@graeme-online.co.uk>
2015-05-24 08:47:27 +0100

    Add explanatory comments to APIFunction

--- docgen/APIFunction.js
+++ docgen/APIFunction.js
@@ -14,6 +14,45 @@
   "use strict";
 
 
+  /*
+   * An APIFunction is an object representing the documentation of a function found in the source code. The constructor
+   * takes the following parameters:
+   *
+   *   name: the name of the function being described as a string.
+   *   category: all documented functions are assumed to be grouped into categories. This string denotes such a
+   *             category.
+   *   summary: a string containing a brief overview of the function's functionality. This is assumed to be a brief
+   *            paragraph. The caller must add any required line endings.
+   *   options: an object containing additional information. See below.
+   *
+   * The supplied category will be transformed to uppercase the first letter of the category name. In other words,
+   * categories "functions" and "Functions" are assumed to be the same category.
+   *
+   * As noted above, additional detail can be supplied in the options object. The following properties are recognised:
+   *   synonyms: An array of strings representing alternate names that can be used to invoke the function. This will
+   *             be an empty array if not supplied.
+   *   returnType: An array of strings representing the possible types that might be returned by an invocation of the
+   *               function. This will be an empty array if not supplied.
+   *   details: An array of strings, each string representing a paragraph of further information about function usage.
+   *            This will be an empty array if not supplied. The caller must add any required line endings.
+   *   examples: An array of strings, each of which represents a line of Javascript illuminating typical usage of the
+   *             function. This will be an empty array if not supplied.
+   *   parameters: See below. This will be an empty array if not supplied.
+   *
+   * The parameters option should be an array of parameters objects. Each such object should have the following
+   * properties:
+   *   name: A string containing the name of the parameter
+   *   type: A non-empty array containing the possible types that the parameter can take
+   *
+   * It is intended that when output to the console, the summary string will be output verbatim: thus it is incumbent
+   * on the client to insert appropriate line endings to ensure that the output looks acceptable.
+   *
+   * Similarly, each entry in the details array is assumed to be a paragraph. It is intended that the in-app help
+   * outputs each paragraph separately, but the client must add line-endings to the individual strings to control line
+   * length.
+   *
+   */
+
   var verifyParameters = function(name, category, summary, options) {
     var argumentTypes = ['string', 'string', 'string', 'object'];
     var typesOK = [].every.call(arguments, function(arg, i) {
